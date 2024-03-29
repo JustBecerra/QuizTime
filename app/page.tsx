0bda6a2b407c9b237cb5900b3fdbd6620a0aab47
JustBecerra <justj.becerra@gmail.com>
2024-03-29 10:32:08 -0300

    fix import issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { Flex } from "@mantine/core";
 import { Header } from "../components/header";
-import { Maininfocard } from "../components/maininfocard";
 import { SearchBar } from "../components/searchbar";
 import { Footer } from "../components/footer";
+import { Maininfocard } from "../components/maininfocard";
 
 export default function HomePage() {
   return (
