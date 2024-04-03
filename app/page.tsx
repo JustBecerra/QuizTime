930fd3213fd96eb7b04480b2a1a8066b66d49d40
JustBecerra <justj.becerra@gmail.com>
2024-04-03 13:58:02 -0300

    remove unnecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import { Flex } from "@mantine/core";
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 import { Maininfocard } from "../components/maininfocard";
-import { getQuestions } from "../api/route";
 
 export default async function HomePage() {
-  // const data = await getQuestions();
   return (
     <Flex
       direction={"column"}
