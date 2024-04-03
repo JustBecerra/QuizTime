5925fe2c11b26f239adf2fea1f07758e18acbd03
JustBecerra <justj.becerra@gmail.com>
2024-04-03 14:45:11 -0300

    add footer and gif

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,5 +1,15 @@
-import { Text } from "@mantine/core";
-
+import { Flex, Text } from "@mantine/core";
+import Image from "next/image";
+import devgif from "../../public/programmergif.gif";
 export const Footer = () => {
-  return <Text>Developed by an awesome developer :D</Text>;
+  return (
+    <Flex
+      align="center"
+      direction="column"
+      style={{ gap: "1rem", marginBottom: "1rem" }}
+    >
+      <Text c="gray.0">Developed by an awesome developer</Text>
+      <Image src={devgif} width={100} height={100} alt={""} />
+    </Flex>
+  );
 };
